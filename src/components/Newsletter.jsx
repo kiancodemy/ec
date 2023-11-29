6a76b389f9c292952d6541d4c7c7419ba64679e7
kian kheirani <kiankheirani@gmail.com>
2023-11-29 15:38:40 +0330

    finished-2

--- src/components/Newsletter.jsx
+++ src/components/Newsletter.jsx
@@ -1,13 +1,13 @@
 function Newsletter() {
   return (
-    <div className="bg-gradient-to-b  from-[#f7bdc5] to-[#f5e3e5] lg:max-w-[800px] rounded-lg p-6 my-14 gap-6 flex flex-col justify-center mx-auto items-center">
+    <div className="bg-gradient-to-b  from-[#f7bdc5] to-[#f5e3e5] lg:max-w-[800px] rounded-lg p-6 my-14 gap-5 flex flex-col justify-center mx-auto items-center">
       <h1 className="capitalize font-semibold text-2xl lg:text-4xl text-[#454545]">
         get exclusive offers on your email
       </h1>
       <p className="lg:text-md text-sm text-[#454545]">
         subscribe to your newsletter and stay updated
       </p>
-      <div className="border-2border-[#bbb] outline-none rounded-full lg:w-[520px] bg-white border-solid border-1 flex justify-between items-center ">
+      <div className="border-2border-[#bbb] outline-none  rounded-full lg:w-[420px] bg-white border-solid border-1 flex justify-center items-center ">
         <input
           type="email"
           className="placeholder:capitalize border-none rounded-full pl-3  focus:outline-none focus:placeholder:translate-x-2 focus:placeholder:text-black  focus:placeholder:duration-300 focus:border-none  "
