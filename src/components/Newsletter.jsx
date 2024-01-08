206b8da7ac3535690e3f0b90f1d900ed9fa76ba4
kian kheirani <kiankheirani@gmail.com>
2024-01-08 16:03:56 +0330

    'add-react-hook-form-login

--- src/components/Newsletter.jsx
+++ src/components/Newsletter.jsx
@@ -1,16 +1,16 @@
 function Newsletter() {
   return (
-    <div className="bg-gradient-to-b  from-[#f7bdc5] to-[#f5e3e5] lg:max-w-[800px] rounded-lg p-6 my-14 gap-5 flex flex-col justify-center mx-auto items-center">
-      <h1 className="capitalize font-semibold text-2xl lg:text-4xl text-[#454545]">
+    <div className="bg-gradient-to-b  from-[#f7bdc5] to-[#f5e3e5] max-w-[900px] rounded-lg p-6 my-14 gap-5 flex flex-col justify-center mx-auto items-center">
+      <h1 className="capitalize text-center font-semibold text-2xl lg:text-4xl text-[#454545]">
         get exclusive offers on your email
       </h1>
-      <p className="lg:text-md text-sm text-[#454545]">
+      <p className="lg:text-md text-sm text-center text-[#454545]">
         subscribe to your newsletter and stay updated
       </p>
-      <div className="border-2border-[#bbb] outline-none w-[80%] mx-auto  rounded-full lg:w-[420px] bg-white border-solid border-1 flex justify-between items-center ">
+      <div className="pl-3 outline-none max-w-[500px] mx-auto bg-white border-solid rounded-full border-1 flex justify-between  ">
         <input
           type="email"
-          className="placeholder:capitalize border-none rounded-full pl-3  focus:outline-none focus:placeholder:translate-x-2 focus:placeholder:text-black  focus:placeholder:duration-300 focus:border-none  "
+          className="placeholder:capitalize border-none rounded-full focus:outline-none focus:placeholder:translate-x-2 focus:placeholder:text-black  focus:placeholder:duration-300 focus:border-none  "
           placeholder="Your email id"
         />
         <button
--- src/pages/Loginsignup.jsx
+++ src/pages/Loginsignup.jsx
@@ -1,30 +1,76 @@
+import { useForm } from "react-hook-form";
+
+import { DevTool } from "@hookform/devtools";
+import { useState, useEffect } from "react";
 function Loginsignup() {
+  const [datas, setdata] = useState();
+  const { register, handleSubmit, formState, watch, control, reset } =
+    useForm();
+  const { errors, isValid, isDirty, isSubmitting, isSubmitSuccessful } =
+    formState;
+
+  const submit = (data) => {
+    console.log("sent");
+    setdata(data);
+    console.log(data);
+  };
+  useEffect(() => {
+    if (isSubmitSuccessful) {
+      reset();
+    }
+  }, [isSubmitSuccessful, reset]);
+
   return (
     <div className="py-40">
-      <div className="border-2 p-8 border-[#ddd] rounded-lg  flex flex-col gap-5 max-w-[500px] mx-auto ">
+      <div className="border-2 p-8 border-[#ddd] rounded-lg  flex flex-col gap-6 max-w-[500px] mx-auto ">
         <h1 className="text-2xl lg:text-4xl font-semibold ">sign up</h1>
 
-        <div className="flex flex-col">
+        <form onSubmit={handleSubmit(submit)} className="flex gap-2  flex-col">
           <input
             type="text"
+            {...register("name", {
+              required: { value: true, message: "fill the field" },
+            })}
             className="border-2 bordre-[#ddd] p-4 focus:placeholder:translate-x-4 focus:placeholder:duration-500  capitalize placeholder:text-black"
             placeholder="Your name"
           />
+          <p className="text-red-500 capitalize animate-bounce py-1">
+            {errors?.name?.message}
+          </p>
           <input
             type="email"
+            {...register("email", {
+              required: {
+                value: /^[^\s@]+@[^\s@]+\.[^\s@]+$/,
+                message: "fill the field",
+              },
+            })}
             className="border-2 bordre-[#ddd] p-4 focus:placeholder:translate-x-4 focus:placeholder:duration-500  capitalize placeholder:text-black"
             placeholder="Email"
           />
+          <p className="text-red-500 animate-bounce capitalize py-1">
+            {errors?.email?.message}
+          </p>
           <input
             type="password"
+            {...register("password", {
+              required: { value: true, message: "fill the password section" },
+            })}
             className="border-2 bordre-[#ddd] p-4 focus:placeholder:translate-x-4 focus:placeholder:duration-500  capitalize placeholder:text-black"
             placeholder="Password"
           />
-        </div>
-
-        <button className="rounded-lg bg-red-600 duration-200 hover:bg-red-700 text-white capitalize py-4">
-          continue
-        </button>
+          <p className="text-red-500 animate-bounce capitalize py-1">
+            {errors?.password?.message}
+          </p>
+          <button
+            disabled={!isDirty && !isValid && isSubmitting}
+            type="submit"
+            className="rounded-lg mt-7 bg-red-600 duration-200 hover:bg-red-700 text-white capitalize py-4"
+          >
+            continue
+          </button>
+        </form>
+        <DevTool control={control}></DevTool>
 
         <p className="flex gap-2 text-sm lg:text-lg ">
           Already have an account ?
